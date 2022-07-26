9ca54d6ad343964d5ca9758c3763b166a51fc93e
Jessica <janash@vt.edu>
2022-07-26 15:20:50 +0000

    add docstring to calculate_distance

--- msf_2022b/functions.py
+++ msf_2022b/functions.py
@@ -3,13 +3,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
+from mpl_toolkits.mplot3d import Axes3D # noqa: F401
 
 
-def calculate_distance(rA, rB):
+def calculate_distance(rA: np.ndarray, rB: np.ndarray) -> np.floating:
+    """
+    Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    dist: np.floating
+        The distance between the two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 1.0, 0])
+    >>> calculate_distance(r1, r2)
+    1.0
+    """
     # This function calculates the distance between two points given as numpy arrays.
     d = rA - rB
-    dist = np.linalg.norm(d)
+    dist: np.floating = np.linalg.norm(d) 
     return dist
 
 
